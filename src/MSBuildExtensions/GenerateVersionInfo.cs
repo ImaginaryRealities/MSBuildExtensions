16044d7c12a9ddcd1027cf39f89ccbf97e013452
Michael Collins <michael@imaginaryrealities.com>
2013-02-14 11:48:17 -0700

    I created the GenerateVersionInfo MSBuild task.
    I created the GenerateVersionInfo class and the GenerateVersionInfoTests
    class to unit test the GenerateVersionInfo class.

---
+++ src/MSBuildExtensions/GenerateVersionInfo.cs
@@ -0,0 +1,146 @@
+﻿//-----------------------------------------------------------------------------
+// <copyright file="GenerateVersionInfo.cs" company="ImaginaryRealities">
+// Copyright 2013 ImaginaryRealities, LLC
+// </copyright>
+// <summary>
+// This file implements the GenerateVersionInfo class. The GenerateVersionInfo
+// class is a custom MSBuild task that will generate a source code file
+// containing the name of the build configuration and the product and assembly
+// version numbers.
+// </summary>
+// <license>
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to
+// deal in the Software without restriction, including without limitation the
+// right to use, copy, modify, merge, publish, distribute, sublicense, and/or
+// sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN N ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+// </license>
+//-----------------------------------------------------------------------------
+
+namespace ImaginaryRealities.MSBuild
+{
+    using System.CodeDom;
+    using System.CodeDom.Compiler;
+    using System.IO;
+    using System.Reflection;
+
+    using Microsoft.Build.Framework;
+    using Microsoft.Build.Utilities;
+
+    /// <summary>
+    /// MSBuild task that will generate a source code file containing the
+    /// build-specific metadata for the project's assemblies.
+    /// </summary>
+    public class GenerateVersionInfo : Task
+    {
+        /// <summary>
+        /// Gets or sets the name of the build configuration for the assembly.
+        /// </summary>
+        /// <value>
+        /// The name of the build configuration (typically Debug or Release).
+        /// </value>
+        [Required]
+        public string AssemblyConfiguration { get; set; }
+
+        /// <summary>
+        /// Gets or sets the version number for the assembly file.
+        /// </summary>
+        /// <value>
+        /// The file version number for the assembly.
+        /// </value>
+        [Required]
+        public string AssemblyFileVersion { get; set; }
+
+        /// <summary>
+        /// Gets or sets the informational version number for the assembly.
+        /// </summary>
+        /// <value>
+        /// The informational version number for the assembly.
+        /// </value>
+        [Required]
+        public string AssemblyInformationalVersion { get; set; }
+
+        /// <summary>
+        /// Gets or sets the version number for the assembly.
+        /// </summary>
+        /// <value>
+        /// The version number for the assembly.
+        /// </value>
+        [Required]
+        public string AssemblyVersion { get; set; }
+
+        /// <summary>
+        /// Gets or sets the path of the source code file to create.
+        /// </summary>
+        /// <value>
+        /// The path of the source code file that the task will generate.
+        /// </value>
+        [Required]
+        public string OutputPath { get; set; }
+
+        /// <summary>
+        /// Gets or sets the semantic version number for the assembly.
+        /// </summary>
+        /// <value>
+        /// The semantic version number that will be added to the assembly
+        /// metadata if this property is specified.
+        /// </value>
+        public string SemanticVersion { get; set; }
+
+        /// <summary>
+        /// Generates the assembly metadata file with the build-specific
+        /// metadata values.
+        /// </summary>
+        /// <returns>
+        /// <b>True</b> if the source code file was successfully generated, or
+        /// <b>false</b> if an error occurred.
+        /// </returns>
+        public override bool Execute()
+        {
+            var codeCompileUnit = new CodeCompileUnit();
+            codeCompileUnit.AssemblyCustomAttributes.Add(
+                new CodeAttributeDeclaration(
+                    new CodeTypeReference(typeof(AssemblyConfigurationAttribute)),
+                    new CodeAttributeArgument(new CodePrimitiveExpression(this.AssemblyConfiguration))));
+            codeCompileUnit.AssemblyCustomAttributes.Add(
+                new CodeAttributeDeclaration(
+                    new CodeTypeReference(typeof(AssemblyFileVersionAttribute)),
+                    new CodeAttributeArgument(new CodePrimitiveExpression(this.AssemblyFileVersion))));
+            codeCompileUnit.AssemblyCustomAttributes.Add(
+                new CodeAttributeDeclaration(
+                    new CodeTypeReference(typeof(AssemblyInformationalVersionAttribute)),
+                    new CodeAttributeArgument(new CodePrimitiveExpression(this.AssemblyInformationalVersion))));
+            codeCompileUnit.AssemblyCustomAttributes.Add(
+                new CodeAttributeDeclaration(
+                    new CodeTypeReference(typeof(AssemblyVersionAttribute)),
+                    new CodeAttributeArgument(new CodePrimitiveExpression(this.AssemblyVersion))));
+            if (!string.IsNullOrEmpty(this.SemanticVersion))
+            {
+                codeCompileUnit.AssemblyCustomAttributes.Add(
+                    new CodeAttributeDeclaration(
+                        "ImaginaryRealities.Framework.SemanticVersionAttribute",
+                        new CodeAttributeArgument(new CodePrimitiveExpression(this.SemanticVersion))));
+            }
+
+            using (var provider = CodeDomProvider.CreateProvider("csharp"))
+            using (var writer = File.CreateText(this.OutputPath))
+            {
+                provider.GenerateCodeFromCompileUnit(codeCompileUnit, writer, new CodeGeneratorOptions());
+            }
+
+            return true;
+        }
+    }
+}
---
+++ src/MSBuildExtensionsUnitTests/UnitTests/GenerateVersionInfoTests.cs
@@ -0,0 +1,107 @@
+﻿//-----------------------------------------------------------------------------
+// <copyright file="GenerateVersionInfoTests.cs" company="ImaginaryRealities">
+// Copyright 2013 ImaginaryRealities, LLC
+// </copyright>
+// <summary>
+// This file implements the unit tests for the GenerateVersionInfo class.
+// </summary>
+// <license>
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to
+// deal in the Software without restriction, including without limitation the
+// right to use, copy, modify, merge, publish, distribute, sublicense, and/or
+// sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN N ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+// </license>
+//-----------------------------------------------------------------------------
+
+namespace ImaginaryRealities.MSBuild.UnitTests
+{
+    using System.IO;
+    using System.Text.RegularExpressions;
+
+    using Xunit;
+
+    /// <summary>
+    /// Unit tests the <see cref="GenerateVersionInfo"/> class.
+    /// </summary>
+    public class GenerateVersionInfoTests
+    {
+        /// <summary>
+        /// The <see cref="GenerateVersionInfo"/> task to test.
+        /// </summary>
+        private readonly GenerateVersionInfo task = new GenerateVersionInfo();
+
+        /// <summary>
+        /// Tests that the <see cref="GenerateVersionInfo"/> task successfully
+        /// generates the version information source code file.
+        /// </summary>
+        [Fact]
+        public void VersionInfoFileIsGeneratedSuccessfully()
+        {
+            string path = null;
+            try
+            {
+                path = Path.GetTempFileName();
+                this.task.OutputPath = path;
+                this.task.AssemblyConfiguration = "Release";
+                this.task.AssemblyFileVersion = "2.1.3.5";
+                this.task.AssemblyInformationalVersion = "2.1.3.5";
+                this.task.AssemblyVersion = "2.0.0.0";
+                this.task.SemanticVersion = "2.1.3+build.5";
+                var result = this.task.Execute();
+                Assert.True(result);
+
+                string sourceCode;
+                using (var reader = File.OpenText(path))
+                {
+                    sourceCode = reader.ReadToEnd();
+                }
+
+                Assert.True(
+                    Regex.IsMatch(
+                        sourceCode,
+                        "\\[assembly: System.Reflection.AssemblyConfigurationAttribute\\(\"Release\"\\)\\]",
+                        RegexOptions.Multiline));
+                Assert.True(
+                    Regex.IsMatch(
+                        sourceCode,
+                        "\\[assembly: System.Reflection.AssemblyFileVersionAttribute\\(\"2\\.1\\.3\\.5\"\\)\\]",
+                        RegexOptions.Multiline));
+                Assert.True(
+                    Regex.IsMatch(
+                        sourceCode,
+                        "\\[assembly: System.Reflection.AssemblyInformationalVersionAttribute\\(\"2\\.1\\.3\\.5\"\\)\\]",
+                        RegexOptions.Multiline));
+                Assert.True(
+                    Regex.IsMatch(
+                        sourceCode,
+                        "\\[assembly: System.Reflection.AssemblyVersionAttribute\\(\"2\\.0\\.0\\.0\"\\)\\]",
+                        RegexOptions.Multiline));
+                Assert.True(
+                    Regex.IsMatch(
+                        sourceCode,
+                        "\\[assembly: ImaginaryRealities.Framework.SemanticVersionAttribute\\(\"2\\.1\\.3\\+build\\.5\"\\)\\]",
+                        RegexOptions.Multiline));
+            }
+            finally
+            {
+                if (null != path && File.Exists(path))
+                {
+                    File.Delete(path);
+                }
+            }
+        }
+    }
+}
